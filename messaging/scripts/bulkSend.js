13a3c199472e4c44dc9e5b9144227df7ad790b08
Marius Obert <obatzer@gmail.com>
2023-10-18 18:52:46 +0200

    file 2

---
+++ messaging/scripts/bulkSend.js
@@ -0,0 +1,9 @@
+/************************************* 
+Run this code to send multiple messages with one API call
+
+Warning: Not yet available publicly
+
+Required: Add the numbers which will communicate
+Docs: 
+ - NA
+*************************************/
